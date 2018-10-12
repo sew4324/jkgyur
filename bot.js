f79a5128bdc50aca4120057b796c287aad3f88b4
sew4324 <43894380+sew4324@users.noreply.github.com>
2018-10-12 17:02:39 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ msg.delete();
 
 
 
-hero.on('message',async message => {
+client.on('message',async message => {
   if(message.author.bot) return;
   if(message.channel.type === 'dm') return;
   let args = message.content.split(' ');
